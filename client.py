537df0d8b270b6e2d01f07b068fd81b155ab3c5c
Prince Modi <princemodi@iisc.ac.in>
2023-06-10 14:44:51 +0530

    Updated client

--- client.py
+++ client.py
@@ -1,21 +1,20 @@
-from collections.abc import AsyncGenerator
-import grpc
 import asyncio
-import echo_pb2, echo_pb2_grpc
+from grpc.aio import insecure_channel
+from echo_pb2 import echoMessage
+from echo_pb2_grpc import EchoServiceStub
 
-clients = ['10.24.24.55','10.24.24.51','10.24.24.50','10.24.24.57','10.24.24.58']
 clients = ['127.0.0.1','10.24.24.57','10.24.24.58']
 r_clients = []
 
 async def grpc_echo(client,channel):
-    stub = echo_pb2_grpc.EchoServiceStub(channel)
+    stub = EchoServiceStub(channel)
     print(f"Sending echo to {client}")
-    response = await stub.Echo(echo_pb2.echoMessage(text=f"{client} responded."))
+    response = await stub.Echo(echoMessage(text=f"{client} responded."))
     r_clients.append(response.text)
 
 async def channel_generator(clients):
     for client in clients:
-        channel = grpc.aio.insecure_channel(f"{client}:50051")
+        channel = insecure_channel(f"{client}:50051")
         yield (client,channel)
 
 async def main(clients):
